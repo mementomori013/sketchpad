ee6338a3217bfbe810042ee622aad4c5bd220462
Memento mori <183689451+mementomori013@users.noreply.github.com>
2025-03-23 00:10:20 +0800

    Modify html, css & javascript file
    Add functions to javascript
    Add styles

--- sketchpad-script.js
+++ sketchpad-script.js
@@ -1,38 +1,93 @@
 const body = document.createElement("body")
 const divContainer = document.querySelector("#container")
 const btn = document.querySelector("#btn")
+const btnContainer = document.querySelector(".button-container")
+const btnGenerate = document.querySelector(".generate")
+let numofSquares = 256;
+let nameofClass = "sixteen"
+let boxColor = "black"
 
-btn.addEventListener("click", ()=>{
-    let num = 0;
-    let numofSquare = Number(prompt("please enter a number: "))
-    num = numofSquare
-    if (numofSquare > 100) {
-        let message = alert("your in input is more than 100 ")
+// btn.addEventListener("click", ()=>{
+//     let num = 0;
+//     let numofSquare = Number(prompt("please enter a number: "))
+//     num = numofSquare
+//     if (numofSquare > 100) {
+//         let message = alert("your in input is more than 100 ")
+//     }
+//      else {
+//         divContainer.innerHTML = ""
+//         generateSquares(num)
+//     }    
+// })
+
+btnContainer.addEventListener("click", (e) => {
+    const class1 = e.target.className
+   
+    switch (class1){
+        case "btn-sixteen":
+            numofSquares = 256;
+            nameofClass = "sixteen"
+            break;
+        case "btn-four":
+            numofSquares = 16;
+            nameofClass = "four";
+            break;
+        case "btn-two":
+            numofSquares = 4;
+            nameofClass = "two";
+            break;
+        case "btn-reset": 
+            divContainer.innerHTML = " "
+            break;
+        case "btn-black": 
+            boxColor = "black"
+            break;
+       case "btn-rainbow": 
+            boxColor = "rainbow"
+            break; 
+        case "generate":
+            divContainer.innerHTML = " "
+            generateSquares(numofSquares, nameofClass); 
+        default:
+          
+
     }
-     else {
-        divContainer.innerHTML = ""
-        generateSquares(num)
-    }
-    
-    
+       
 })
 
-function generateSquares(num){
-for (let i = 0; i < num; i++){
-    const div = document.createElement("div")
-    divContainer.appendChild(div)
-    for (let j = 0; j < num; j++){
-        const div = document.createElement("div")
-        divContainer.appendChild(div)
+btnGenerate.addEventListener("click", (e) => { 
+    console.log(numofSquares)
       
-    }
-  
-}
-}
+})
 
+function generateSquares(num, className){
+   
+    console.log("console " + nameofClass)
+    for (let i = 0; i < num; i++){
+        const div = document.createElement("div")
+        div.className = className
+        
+        divContainer.appendChild(div)
+    }   
+}
 
 divContainer.addEventListener("mouseover", (e) => {
-    e.target.className = "changeColor"
-  
+    console.log(nameofClass)
+    switch (boxColor){
+        case "black":
+            if(e.target.className == nameofClass){
+                console.log(e.target.className)
+                e.target.style.backgroundColor = "black"
+            }
+        break;
+        case "rainbow":
+        const randomColor = Math.floor(Math.random()*16777215).toString(16)
+        if(e.target.className == nameofClass){
+        e.target.style.backgroundColor = "#" + randomColor
+        }
+        break;
+    }
 })
+
+
 
